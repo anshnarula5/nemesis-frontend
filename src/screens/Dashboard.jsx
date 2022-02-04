28d2142e389d32ea641d04801949e151a776cc03
Ansh Narula <2019210@iiitdmj.ac.in>
2022-02-04 18:02:03 +0530

    bug fixes2

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -11,7 +11,6 @@ import {
 } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-import { setAlert } from "../redux/actions/alertAction";
 import { addUser, deleteUser, getAllUsers } from "../redux/actions/userActions";
 
 const Dashboard = () => {
